0399fa9736b5a9604b3dfa3c8f6fabb29b3147a0
eevajonnapanula <eeva-jonna.panula@codento.com>
2020-03-15 18:57:38 +0200

    Updates

---
+++ precache-manifest.e4e8b71f9084179f474b663326d6c6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b70c51974867a0435036b411c2a0a9",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "83688c05bbc4a6d03992",
+    "url": "/covid-19/static/css/main.3d25a231.chunk.css"
+  },
+  {
+    "revision": "03fc0d3f72c03a1717e9",
+    "url": "/covid-19/static/js/2.f7f7c43c.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/covid-19/static/js/2.f7f7c43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83688c05bbc4a6d03992",
+    "url": "/covid-19/static/js/main.429246a0.chunk.js"
+  },
+  {
+    "revision": "e6f5ab8ee53266f60879",
+    "url": "/covid-19/static/js/runtime-main.d9e1be68.js"
+  }
+]);
